f57ab6760f8883dd684ec28c801f5cfb6f5cb836
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2016-04-17 11:59:11 -0700

    return true if divisible by 400 or 4, but not 100. final spec

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 //alert("jQuery is working on " + $("h1").text()); //test for jQuery linked and loaded correctly
 
 function leapYear(year) {
-  if ((year%4===0) && (year%100===0)){
+  if ((year%4===0) && ((year%100!==0) || (year%400===0))){
     return true;
   } else {
     return false;
