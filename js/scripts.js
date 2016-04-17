a096c2f07d5ffbe30dc0995a57a083d8ae4454be
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2016-04-17 11:55:17 -0700

    clean up and simplify code

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,7 @@
 //alert("jQuery is working on " + $("h1").text()); //test for jQuery linked and loaded correctly
 
 function leapYear(year) {
-  if (year%100===0){
-    return false;
-  } else if (year%4===0){
+  if ((year%4===0) && (year%100===0)){
     return true;
   } else {
     return false;
